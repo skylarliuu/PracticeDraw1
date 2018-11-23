48ae7a024a5a80fe29037c3010141534cc5b9461
skylarliuu <skylarliuu@163.com>
2018-11-23 16:38:00 +0800

    完成饼图绘制

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
@@ -194,52 +194,164 @@
 //}
 
 
+//package com.hencoder.hencoderpracticedraw1.practice;
+//
+//import android.content.Context;
+//import android.graphics.Canvas;
+//import android.graphics.Color;
+//import android.graphics.Paint;
+//import android.graphics.Path;
+//import android.graphics.Point;
+//import android.graphics.Rect;
+//import android.graphics.RectF;
+//import android.support.annotation.Nullable;
+//import android.util.AttributeSet;
+//import android.view.View;
+//
+//
+//import com.hencoder.hencoderpracticedraw1.Data;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class Practice11PieChartView extends View {
+//
+//    private float radius;
+//    private List<Data> datas;
+//    private Paint paint;
+//    private RectF rectF;
+//    private float total;
+//    private float max;
+//
+//    float startAngle = 0f; // 开始的角度
+//    float sweepAngle;      // 扫过的角度
+//    float lineAngle;       // 当前扇形一半的角度
+//
+//    float lineStartX = 0f; // 直线开始的X坐标
+//    float lineStartY = 0f; // 直线开始的Y坐标
+//    float lineEndX;        // 直线结束的X坐标
+//    float lineEndY;        // 直线结束的Y坐标
+//
+//    public Practice11PieChartView(Context context) {
+//        super(context);
+//        init();
+//    }
+//
+//    public Practice11PieChartView(Context context, @Nullable AttributeSet attrs) {
+//        super(context, attrs);
+//        init();
+//    }
+//
+//    public Practice11PieChartView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+//        super(context, attrs, defStyleAttr);
+//        init();
+//    }
+//
+//    private void init() {
+//        radius = 300;
+//        datas = new ArrayList<>();
+//        Data data = new Data("Gingerbread", 10.0f, Color.WHITE);
+//        datas.add(data);
+//        data = new Data("Ice Cream Sandwich", 18.0f, Color.MAGENTA);
+//        datas.add(data);
+//        data = new Data("Jelly Bean", 22.0f, Color.GRAY);
+//        datas.add(data);
+//        data = new Data("KitKat", 27.0f, Color.GREEN);
+//        datas.add(data);
+//        data = new Data("Lollipop", 40.0f, Color.BLUE);
+//        datas.add(data);
+//        data = new Data("Marshmallow", 30.0f, Color.RED);
+//        datas.add(data);
+//        data = new Data("Nougat", 63.5f, Color.YELLOW);
+//        datas.add(data);
+//        total = 0.0f;
+//        max = Float.MIN_VALUE;
+//        for (Data d : datas) {
+//            total += d.getNumber();
+//            max = Math.max(max, d.getNumber());
+//        }
+//        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+//        paint.setStrokeWidth(2);
+//        paint.setTextSize(30);
+//        rectF = new RectF(-300, -300, 300, 300);
+//    }
+//
+//    @Override
+//    protected void onDraw(Canvas canvas) {
+//        super.onDraw(canvas);
+//        canvas.translate(canvas.getWidth() / 2, canvas.getHeight() / 2);  // 将画布(0，0)坐标点移到画布的中心
+//        startAngle = 0f; //这句代码很重要，不然有bug
+//        for (Data data : datas) {
+//            paint.setStyle(Paint.Style.FILL);
+//            paint.setColor(data.getColor());
+//            sweepAngle = data.getNumber() / total * 360f;
+//            lineAngle = startAngle + sweepAngle / 2;
+//            lineStartX = radius * (float) Math.cos(lineAngle / 180 * Math.PI);
+//            lineStartY = radius * (float) Math.sin(lineAngle / 180 * Math.PI);
+//            lineEndX = (radius + 50) * (float) Math.cos(lineAngle / 180 * Math.PI);
+//            lineEndY = (radius + 50) * (float) Math.sin(lineAngle / 180 * Math.PI);
+//            if (data.getNumber() == max) {
+//                canvas.save();
+//                canvas.translate(lineStartX * 0.1f, lineStartY * 0.1f);
+//                canvas.drawArc(rectF, startAngle, sweepAngle, true, paint);
+//            } else {
+//                canvas.drawArc(rectF, startAngle, sweepAngle - 1.0f, true, paint);
+//            }
+//            paint.setColor(Color.WHITE);
+//            paint.setStyle(Paint.Style.STROKE);
+//            canvas.drawLine(lineStartX, lineStartY, lineEndX, lineEndY, paint);
+//            if (lineAngle > 90 && lineAngle <= 270) {
+//                canvas.drawLine(lineEndX, lineEndY, lineEndX - 50, lineEndY, paint);
+//                paint.setStyle(Paint.Style.FILL);
+//                canvas.drawText(data.getName(), lineEndX - 50 - 10 - paint.measureText(data.getName()), lineEndY, paint);
+//            } else {
+//                canvas.drawLine(lineEndX, lineEndY, lineEndX + 50, lineEndY, paint);
+//                paint.setStyle(Paint.Style.FILL);
+//                canvas.drawText(data.getName(), lineEndX + 50 + 10, lineEndY, paint);
+//            }
+//            if (data.getNumber() == max) {
+//                canvas.restore();
+//            }
+//            startAngle += sweepAngle;
+//        }
+//    }
+//}
+
+
 package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Path;
-import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
-
 import com.hencoder.hencoderpracticedraw1.Data;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Practice11PieChartView extends View {
 
-    private float radius;
-    private List<Data> datas;
-    private Paint paint;
-    private RectF rectF;
-    private float total;
-    private float max;
+    private ArrayList<Data> datas;
+    private float radius = 300f;
+    private float maxNum = 0f;
+    private float total = 0f;
+    private RectF rect;
+    private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private float startAngel,sweepAngel,lineAngel = 0f;
+    private float lineStartX,lineEndX,lineStartY,lineEndY = 0f;
 
-    float startAngle = 0f; // 开始的角度
-    float sweepAngle;      // 扫过的角度
-    float lineAngle;       // 当前扇形一半的角度
-
-    float lineStartX = 0f; // 直线开始的X坐标
-    float lineStartY = 0f; // 直线开始的Y坐标
-    float lineEndX;        // 直线结束的X坐标
-    float lineEndY;        // 直线结束的Y坐标
 
     public Practice11PieChartView(Context context) {
-        super(context);
-        init();
+        this(context,null);
     }
 
     public Practice11PieChartView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        init();
+        this(context, attrs,0);
     }
 
     public Practice11PieChartView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -248,7 +360,6 @@ public class Practice11PieChartView extends View {
     }
 
     private void init() {
-        radius = 300;
         datas = new ArrayList<>();
         Data data = new Data("Gingerbread", 10.0f, Color.WHITE);
         datas.add(data);
@@ -264,55 +375,65 @@ public class Practice11PieChartView extends View {
         datas.add(data);
         data = new Data("Nougat", 63.5f, Color.YELLOW);
         datas.add(data);
-        total = 0.0f;
-        max = Float.MIN_VALUE;
-        for (Data d : datas) {
-            total += d.getNumber();
-            max = Math.max(max, d.getNumber());
+
+        //找到最大值
+        for(Data data1:datas){
+            maxNum = Math.max(data1.getNumber(),maxNum);
+            total += data1.getNumber();
         }
-        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setStrokeWidth(2);
-        paint.setTextSize(30);
-        rectF = new RectF(-300, -300, 300, 300);
+        rect = new RectF(-300,-300,300,300);
+        mPaint.setStrokeWidth(2);
+        mPaint.setTextSize(24);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.translate(canvas.getWidth() / 2, canvas.getHeight() / 2);  // 将画布(0，0)坐标点移到画布的中心
-        startAngle = 0f; //这句代码很重要，不然有bug
-        for (Data data : datas) {
-            paint.setStyle(Paint.Style.FILL);
-            paint.setColor(data.getColor());
-            sweepAngle = data.getNumber() / total * 360f;
-            lineAngle = startAngle + sweepAngle / 2;
-            lineStartX = radius * (float) Math.cos(lineAngle / 180 * Math.PI);
-            lineStartY = radius * (float) Math.sin(lineAngle / 180 * Math.PI);
-            lineEndX = (radius + 50) * (float) Math.cos(lineAngle / 180 * Math.PI);
-            lineEndY = (radius + 50) * (float) Math.sin(lineAngle / 180 * Math.PI);
-            if (data.getNumber() == max) {
+
+//        综合练习
+//        练习内容：使用各种 Canvas.drawXXX() 方法画饼图
+        canvas.translate(canvas.getWidth()/2,canvas.getHeight()/2);
+        startAngel = 0f;
+        for(Data data:datas){
+            mPaint.setStyle(Paint.Style.FILL);
+            mPaint.setColor(data.getColor());
+
+            sweepAngel = data.getNumber() / total * 360f;
+            lineAngel = startAngel + sweepAngel / 2;//扇形中点对应的角度
+            lineStartX = (float) (radius * Math.cos(lineAngel / 180f * Math.PI));
+            lineStartY = (float) (radius * Math.sin(lineAngel / 180f * Math.PI));
+            lineEndX = (float) ((radius + 50) * Math.cos(lineAngel / 180f * Math.PI));
+            lineEndY = (float) ((radius + 50) * Math.sin(lineAngel / 180f * Math.PI));
+
+            //画扇形
+            if(data.getNumber() == maxNum){//最大的扇形要突出一点
                 canvas.save();
-                canvas.translate(lineStartX * 0.1f, lineStartY * 0.1f);
-                canvas.drawArc(rectF, startAngle, sweepAngle, true, paint);
-            } else {
-                canvas.drawArc(rectF, startAngle, sweepAngle - 1.0f, true, paint);
+                canvas.translate(lineStartX * 0.1f,lineStartY * 0.1f);
+                canvas.drawArc(rect,startAngel,sweepAngel,true,mPaint);
+            }else{
+                canvas.drawArc(rect,startAngel,sweepAngel - 2f,true,mPaint);
             }
-            paint.setColor(Color.WHITE);
-            paint.setStyle(Paint.Style.STROKE);
-            canvas.drawLine(lineStartX, lineStartY, lineEndX, lineEndY, paint);
-            if (lineAngle > 90 && lineAngle <= 270) {
-                canvas.drawLine(lineEndX, lineEndY, lineEndX - 50, lineEndY, paint);
-                paint.setStyle(Paint.Style.FILL);
-                canvas.drawText(data.getName(), lineEndX - 50 - 10 - paint.measureText(data.getName()), lineEndY, paint);
-            } else {
-                canvas.drawLine(lineEndX, lineEndY, lineEndX + 50, lineEndY, paint);
-                paint.setStyle(Paint.Style.FILL);
-                canvas.drawText(data.getName(), lineEndX + 50 + 10, lineEndY, paint);
+
+            mPaint.setColor(Color.WHITE);
+            mPaint.setStyle(Paint.Style.STROKE);
+            canvas.drawLine(lineStartX,lineStartY,lineEndX,lineEndY,mPaint);
+            if(lineAngel >90 && lineAngel <270){//圆的左半部分
+                canvas.drawLine(lineEndX,lineEndY,lineEndX - 50,lineEndY,mPaint);
+                mPaint.setStyle(Paint.Style.FILL);
+                canvas.drawText(data.getName(),lineEndX - 50 - 10 - mPaint.measureText(data.getName()),lineEndY,mPaint);
+            }else{//圆的右半部分
+                canvas.drawLine(lineEndX,lineEndY,lineEndX + 50,lineEndY,mPaint);
+                mPaint.setStyle(Paint.Style.FILL);
+                canvas.drawText(data.getName(),lineEndX + 50 + 10 ,lineEndY,mPaint);
             }
-            if (data.getNumber() == max) {
+
+            if(data.getNumber() == maxNum){
                 canvas.restore();
             }
-            startAngle += sweepAngle;
+
+            startAngel += sweepAngel;
+
         }
+
     }
 }
